d7f7d6b67103dc055df1db41001fb4d37114eef7
VK432 <VK14831@my.bristol.ac.uk>
2015-11-28 15:28:41 +0000

    inital commit

--- .build/src/main.xc.d
+++ .build/src/main.xc.d
@@ -1,4 +1,4 @@
 .build/src//main.xc.d ./.build/src//main.xc.o ./.build/src//main.xc.pca.xml: ./src/main.xc \
-  ../../COMS20001/game_of_life/src/pgmIO.h \
-  ../../COMS20001/lib_i2c/api/i2c.h
+  /Users/Varadhan/Google\ Drive/Game_of_Life/GameOfLife/src/pgmIO.h \
+  /Users/Varadhan/Google\ Drive/Game_of_Life/GameOfLife/src/i2c.h
 
